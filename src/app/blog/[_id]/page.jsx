28a5555442a99a34c2a4d6cb9b824a40971f2189
Rishikesh Sharma <wrongdirection72@gmail.com>
2023-05-16 17:10:24 +0545

    com

--- src/app/blog/[_id]/page.jsx
+++ src/app/blog/[_id]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Blog from "@/components/pages/blog";
+import Blog from "../../../components/sections/Blog";
 export default function page({ params, searchParams }) {
   return <Blog _id={params._id} />;
 }
